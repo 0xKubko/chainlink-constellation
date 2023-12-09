4a80b06a019403c591d35ff8660b0605ad306fcf
kubko <kubko@redacted.finance>
2023-12-09 21:57:32 +0000

    chore: filetree cleanup

--- script/Example02.s.sol
+++ script/Example02.s.sol
@@ -30,67 +30,6 @@ contract DeployBasicMessageReceiver is Script, Helper {
     }
 }
 
-contract CCIPTokenTransfer is Script, Helper {
-    function run(
-        SupportedNetworks source,
-        SupportedNetworks destination,
-        address basicMessageReceiver,
-        address tokenToSend,
-        uint256 amount,
-        PayFeesIn payFeesIn
-    ) external returns (bytes32 messageId) {
-        uint256 senderPrivateKey = vm.envUint("PRIVATE_KEY");
-        vm.startBroadcast(senderPrivateKey);
-
-        (address sourceRouter, address linkToken, , ) = getConfigFromNetwork(
-            source
-        );
-        (, , , uint64 destinationChainId) = getConfigFromNetwork(destination);
-
-        IERC20(tokenToSend).approve(sourceRouter, amount);
-
-        Client.EVMTokenAmount[]
-            memory tokensToSendDetails = new Client.EVMTokenAmount[](1);
-        Client.EVMTokenAmount memory tokenToSendDetails = Client
-            .EVMTokenAmount({token: tokenToSend, amount: amount});
-
-        tokensToSendDetails[0] = tokenToSendDetails;
-
-        Client.EVM2AnyMessage memory message = Client.EVM2AnyMessage({
-            receiver: abi.encode(basicMessageReceiver),
-            data: "",
-            tokenAmounts: tokensToSendDetails,
-            extraArgs: "",
-            feeToken: payFeesIn == PayFeesIn.LINK ? linkToken : address(0)
-        });
-
-        uint256 fees = IRouterClient(sourceRouter).getFee(
-            destinationChainId,
-            message
-        );
-
-        if (payFeesIn == PayFeesIn.LINK) {
-            IERC20(linkToken).approve(sourceRouter, fees);
-            messageId = IRouterClient(sourceRouter).ccipSend(
-                destinationChainId,
-                message
-            );
-        } else {
-            messageId = IRouterClient(sourceRouter).ccipSend{value: fees}(
-                destinationChainId,
-                message
-            );
-        }
-
-        console.log(
-            "You can now monitor the status of your Chainlink CCIP Message via https://ccip.chain.link using CCIP Message ID: "
-        );
-        console.logBytes32(messageId);
-
-        vm.stopBroadcast();
-    }
-}
-
 contract GetLatestMessageDetails is Script, Helper {
     function run(address basicMessageReceiver) external view {
         (
